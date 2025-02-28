b2261bc7f6926021c2a7527647785311536c7679
Sounikc <sounikchakraborty777@gmail.com>
2025-02-28 11:58:19 +0530

    changed todo component

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -3,8 +3,16 @@ import todoReducer from "./toDoReducer";
 
 const ToDo = () => {
     const inititalState = [];
+    const defaultTodos = [
+        {id: 0, taskname: '!!Say Good Morning to all!! ', isCompleted: false},
+        {id: 1, taskname: 'Give VitD3 Medicine!! ', isCompleted: false},
+        {id: 2, taskname: 'Provide Body Lotion!! ', isCompleted: false},
+        {id: 3, taskname: 'Wash Feeding Bottle!! ', isCompleted: false},
+        {id: 4, taskname: 'Give Tofu Bath!! ', isCompleted: false},
+        {id: 5, taskname: 'Give Tofu Zyncovit!! ', isCompleted: false},
+    ]
     const init = (inititalState) => {
-        return [...inititalState, {id: 0, taskname: '!!Say Good Morning to all!! ', isCompleted: false}]
+        return [...inititalState, ...defaultTodos]
     }
     const [currentTodo, setCurrentTodo] = useState();
     const [todos, dispatch] = useReducer(todoReducer, inititalState, init);
