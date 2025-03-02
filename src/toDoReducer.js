786037e58bfd4a97914db10d4f280c94bdab0474
Sounikc <sounikchakraborty777@gmail.com>
2025-03-02 09:26:34 +0530

    added ignore file and add delete operation using lambda

--- src/toDoReducer.js
+++ src/toDoReducer.js
@@ -1,29 +1,37 @@
-const todoReducer = (todos, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            const { id, taskname, isCompleted } = action.payload;
-            return (
-                [
-                    ...todos, {
-                        id: id,
-                        taskname: taskname,
-                        isCompleted: isCompleted
-                    }
-                ]
-            )
-        case 'REMOVE_TODO':
-            return (
-                [...todos].filter((todo) => {
-                    return todo != action.payload
-                })
-            )
-        case 'MARK_TODO':
-            const todoItems = [...todos];
-            todoItems[action.payload.id].isCompleted = true;
-            return todoItems;
-        default:
-            return todos;
-    }
-}
+// export const initializer = (inititalState) => {
+//   const defaultTodos = [
+//     { id: 0, taskname: "!!Say Good Morning to all!! ", isCompleted: false },
+//     { id: 1, taskname: "Give VitD3 Medicine!! ", isCompleted: false },
+//     { id: 2, taskname: "Provide Body Lotion!! ", isCompleted: false },
+//     { id: 3, taskname: "Wash Feeding Bottle!! ", isCompleted: false },
+//     { id: 4, taskname: "Give Tofu Bath!! ", isCompleted: false },
+//     { id: 5, taskname: "Give Tofu Zyncovit!! ", isCompleted: false },
+//   ];
+//   return { ...inititalState, items: defaultTodos };
+// };
 
-export default todoReducer;
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_INITIAL_TODO":
+      return { ...state, items: action.payload };
+    case "ADD_TODO":
+    //   const { id, taskname, isCompleted } = action.payload;
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    case "REMOVE_TODO":
+      return {
+        ...state,
+        items: state.items.filter((item) => {
+            return item.id != action.payload.id;
+        })
+      }
+    // case "MARK_TODO":
+    //   const todoItems = [...todos];
+    //   todoItems[action.payload.id].isCompleted = true;
+    //   return todoItems;
+    default:
+      return todos;
+  }
+};
